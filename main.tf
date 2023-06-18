7cb4b412c0cf89b5450670cd03ab829ab70dc315
marinamagaldi <marinamagaldib@hotmail.com>
2023-06-18 10:49:51 -0300

    Criando bucket S3 via Terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "marinamagaldi"
+      managed-by = "terraform"
+    }
+  }
+}
